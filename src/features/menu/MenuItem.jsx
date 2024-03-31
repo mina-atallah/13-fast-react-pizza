54a5855290e7e6e71daab4cd0d13ec46cc3ea377
mina-atallah <minaatallah18@gmail.com>
2024-03-31 13:37:42 +0200

    Display Loader Indicator - created Loader component. - used useNavigation hook  in the app component to know page status and to display the Lodaer when status is "loading"  when choose a specific route. - replaced imageUrl with local image because images cannot be fetched .

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,15 +1,16 @@
 import { formatCurrency } from "../../utils/helpers";
+// i got a pizza img to replace the imageUrl coming from api because of (err.name-not-resolved error)
+import "../../../src/pizza.jpg";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
     <li>
-      <img src={imageUrl} alt={name} />
+      {/* <img src={imageUrl} alt={name} /> */}
+      <img src="/src/pizza.jpg" alt={name} />
       <div>
-        <p>
-          {name} - <span>{id}</span>
-        </p>
+        <p>{name}</p>
         <p>{ingredients.join(", ")}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,18 @@
-import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <div className="layout">
+      {isLoading && <Loader />}
       <Header />
 
       <main>
-        <h1>CONTENT</h1>
         <Outlet />
       </main>
 

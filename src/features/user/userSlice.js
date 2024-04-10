f13b043af79c946571ea5bc640ba8e83ead1893d
mina-atallah <minaatallah18@gmail.com>
2024-04-11 00:00:10 +0200

    Model User State with Redux-Toolkit - installed "redux/toolkit" to handle and create global state & "react-redux" to extract the needed values from Redux. - created store for global state. - created the userSlice as a slice of the global store. - provided the state using Provider component.

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,3 +1,6 @@
+/*
+import { getAddress } from "../../services/apiGeocoding";
+
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -19,3 +22,25 @@ async function fetchAddress() {
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
 }
+*/
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userName: "",
+};
+
+// Created A Slice of Global State
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.userName = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;

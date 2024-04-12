2946b22bfd65da6327f587577744c21001fb1e8c
mina-atallah <minaatallah18@gmail.com>
2024-04-12 09:36:51 +0200

    Add Menu Items to Cart - created handle func to handle & create new item, then calling the disptach function to call actionCreator function that adds the needed item to the cart. - passed onClick prop to the Button component to handle events.

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,23 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const disptach = useDispatch();
+
+  function handleAddToCart() {
+    const newItem = {
+      id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    disptach(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-3 py-2">
@@ -25,7 +40,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>

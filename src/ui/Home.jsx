db6b9c1176fff0eaf48edafdd4b441d95b31b23d
mina-atallah <minaatallah18@gmail.com>
2024-04-12 03:31:08 +0200

    Read & Update The User State - updated the user.username state by calling the disptach action creating function in handleSubmit handler function. - read the user state value calling the userSelector hook that access the store's state. - handled the case if there is a username and the user returned to the home page can go back to the menu page without the need to re-write the name agian.

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,9 @@
 import CreateUser from "../features/user/CreateUser";
+import { useSelector } from "react-redux";
+import Button from "./Button";
 
 function Home() {
+  const userName = useSelector((state) => state.user.userName);
   return (
     <div className="my-9 text-center sm:my-16">
       <h1 className="mb-8 px-4 text-xl font-semibold md:text-3xl">
@@ -10,7 +13,14 @@ function Home() {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+
+      {userName === "" ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          continue ordering, {userName}
+        </Button>
+      )}
     </div>
   );
 }
